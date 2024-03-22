14a8c3338bea83dbcb1e033ec9751e6869420d32
Archit Kakkar <education.archit@gmail.com>
2024-03-22 17:08:48 +0530

    added logic to get random meme on button click.

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Meme from './components/Meme'
 
 function App() {
   return (
-    <div className='flex items-center justify-center w-screen h-screen bg-zinc-200'>
-      <div className='font-karla w-[550px] h-[550px] shadow-xl bg-white rounded-sm'>
+    <div className='flex items-center justify-center w-screen h-auto min-h-screen bg-zinc-200'>
+      <div className='font-karla w-[550px] my-4 h-auto shadow-xl bg-white rounded-sm'>
         <Navbar />
         <Meme />
       </div>
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import memesData from '../memesData'
 
-function Form() {
+function Meme() {
+    console.log()
+
+    function GetNewMeme() {
+        const memesArray = memesData.data.memes;
+        const randomNum = Math.floor(Math.random() * memesArray.length)
+        const randomMeme = memesArray[randomNum]
+        const URL = randomMeme.url
+
+        document.querySelector(".meme").setAttribute("src", URL);
+
+        console.log(URL)
+    }
+
     return (
         <>
             <main className='px-6 py-8'>
-                <form action="" className='flex flex-col gap-4'>
+                <div className='flex flex-col gap-4'>
                     <div className="flex items-start justify-between gap-8">
                         <div className="flex flex-col w-full">
                             <label htmlFor="top" className='text-sm font-semibold'>Top Text</label>
@@ -15,14 +29,14 @@ function Form() {
                             <input type="text" id='bottom' placeholder='enter text...' className='border border-[#D1D5DB] rounded-md px-3 py-1 mt-1' />
                         </div>
                     </div>
-                    <button type="submit" className='py-2.5 text-white bg-gradient-to-r from-[#672280] to-[#A626D3] font-bold rounded-md'>Get a new meme image 🖼️</button>
-                </form>
+                    <button type="submit" onClick={GetNewMeme} className='py-2.5 text-white bg-gradient-to-r from-[#672280] to-[#A626D3] font-bold rounded-md hover:opacity-90'>Get a new meme image 🖼️</button>
+                </div>
             </main>
-            <section className='mx-6 border border-black rounded-sm'>
-                <img src="" alt="meme-here" className='h-64' />
+            <section className='mx-6 mb-10 border border-black rounded-sm '>
+                <img src="" alt="meme-here" className='object-contain w-full h-full meme ' />
             </section>
         </>
     )
 }
 
-export default Form
+export default Meme
